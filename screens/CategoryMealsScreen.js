e336a44cae239ce8ecb10e804522f390b11bec77
Kingsley Ramos <Kingsleyramos@gmail.com>
2020-08-09 23:24:55 -0700

    added mealItem and buttons

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,24 +1,41 @@
 import React from 'react';
 import {View, Text, FlatList, StyleSheet} from 'react-native';
+import { HeaderButtons } from 'react-navigation-header-buttons' // Note this is HeaderButtons not HeaderButton. Note this is plural
 
 import {CATEGORIES, MEALS} from '../data/dummy-data';
+import MealItem from '../components/MealItem';
 
 const CategoryMealScreen = (props) => {
     // receive the data passed by CategoriesScreen
 
     const renderMealItem = (itemData) => {
         return (
-            <View>
-                <Text>{itemData.item.title}</Text>
-            </View>
+            <MealItem
+                title={itemData.item.title}
+                image={itemData.item.imageUrl}
+                duration={itemData.item.duration}
+                complexity={itemData.item.complexity}
+                affordability={itemData.item.affordability}
+                onSelectMeal={() => {
+                    // Navigate to the MealDetailScreen
+                    props.navigation.navigate({
+                        routeName: 'MealDetail',
+                        // This will pass the meal ID to the MealDetailScreen
+                        params: {
+                            mealId: itemData.item.id,
+                        },
+                    });
+                }}
+            />
         );
     };
 
     const catId = props.navigation.getParam('categoryId');
 
-    const displayedMeals = MEALS.filter((meal) => 
-        // this will be -1 if the category ID is not part of the category IDs
-        meal.categoryIds.indexOf(catId) >= 0
+    const displayedMeals = MEALS.filter(
+        (meal) =>
+            // this will be -1 if the category ID is not part of the category IDs
+            meal.categoryIds.indexOf(catId) >= 0
     );
 
     return (
@@ -27,6 +44,7 @@ const CategoryMealScreen = (props) => {
                 data={displayedMeals}
                 keyExtractor={(item, index) => item.id}
                 renderItem={renderMealItem}
+                style={{width: '100%'}}
             />
         </View>
     );
@@ -46,6 +64,7 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
+        padding: 15,
     },
 });
 
