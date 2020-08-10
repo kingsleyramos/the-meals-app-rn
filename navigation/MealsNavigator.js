25d76541aa231d84418c88b44d56350bcd80a5d4
Kingsley Ramos <Kingsleyramos@gmail.com>
2020-08-09 23:43:12 -0700

    added tab navigator

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,11 +1,15 @@
 import {createAppContainer} from 'react-navigation';
 import {Platform} from 'react-native'
 import {createStackNavigator} from 'react-navigation-stack';
+import {createBottomTabNavigator} from 'react-navigation-tabs';
 
 import CategoriesScreen from '../screens/CategoriesScreen';
 import CategoryMealsScreen from '../screens/CategoryMealsScreen';
 import MealDetailScreen from '../screens/MealDetailScreen';
+import FavoritesScreen from '../screens/FavoritesScreen'
+
 import Colors from '../constants/Colors'
+
 
 const MealsNavigator = createStackNavigator({
     Categories: CategoriesScreen,
@@ -33,4 +37,9 @@ const MealsNavigator = createStackNavigator({
     }
 });
 
-export default createAppContainer(MealsNavigator);
+const MealsFavTabNavigator = createBottomTabNavigator({
+    Meals: MealsNavigator,
+    Favorites: FavoritesScreen
+});
+
+export default createAppContainer(MealsFavTabNavigator);
